55d9bce4d95beea6aaeeafba5f1b21f987094e57
Stacy Areas <stacyareas@gmail.com>
2017-08-24 22:16:41 -0700

    edit description of projects

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -17,7 +17,7 @@ angular.module('portfolio.projects', ['ngRoute'])
 			github: 'https://github.com/stacy89/Student-Management-Tool',
 			image: 'public/Projects/MGMT.png',
 			description: "An administrative system to store student data and generate student groups based on criteria like GPA, gender, and attendance. Formulated the students routes and used CSS to design the pages. This tool is useful for teachers that have a hard time forming groups of their students and to keep track of what student belongs to what teacher. [Temp. access]--[email: teacher@teacher.com | password: teacher]",
-			tech: "Tech: Ruby on Rails, PostgreSQL, Heroku, Paperclip Gem, Twilio API"
+			tech: "Tech: Ruby on Rails, Ruby, PostgreSQL, Heroku, Twilio API"
 		},
 		{
 			github: 'https://github.com/stacy89/Lost_and_Found',
@@ -30,7 +30,7 @@ angular.module('portfolio.projects', ['ngRoute'])
 			github: 'https://github.com/stacy89/Authentic-Realities',
 			image: 'public/Projects/AuthenticRealities.png',
 			description: "Data visualization using VR to express data/statistics of inequalities in salary based on personal identity. Created the main visual environment and helped with coding the triggered events on collision. This experience is offered to help users feels an emotional connection to the lived experience of socioeconomic status.",
-			tech: "Tech: Unity, C#, MonoDevelop, GitHub, Heroku, SteamVR, Oculus"
+			tech: "Tech: Unity, C#, MonoDevelop, Heroku, SteamVR, Oculus"
 		}
 	];
 }]);
