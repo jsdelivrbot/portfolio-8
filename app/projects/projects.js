b4bc13167b131f6e7dad24b4b58580ceaaa1a813
Stacy Areas <stacyareas@gmail.com>
2017-10-12 15:03:45 -0700

    remove old css and replace with responsive layout

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -13,19 +13,22 @@ angular.module('portfolio.projects', ['ngRoute'])
 
 	$scope.projects = [
 		{
+			name: 'Student MGMT',
 			live: 'https://teacherstudentmanagement.herokuapp.com/',
 			github: 'https://github.com/stacy89/Student-Management-Tool',
 			image: 'public/Projects/MGMT.png',
-			description: "An administrative system to store student data and generate student groups based on criteria like GPA, gender, and attendance. Formulated the students routes and used CSS to design the pages. This tool is useful for teachers that have a hard time forming groups of their students and to keep track of what student belongs to what teacher. [Temp. access]--[email: teacher@teacher.com | password: teacher]",
+			description: "An administrative system to store student data and generate student groups based on criteria like GPA, gender, and attendance. Formulated the students routes and used CSS to design the pages. This tool is useful for teachers that have a hard time forming groups of their students and to keep track of what student belongs to what teacher.   [Temp. access]--[email: teacher@teacher.com | password: teacher]",
 			tech: "Tech: Ruby on Rails, Ruby, PostgreSQL, Heroku, Twilio API"
 		},
 		{
+			name: 'Lost and Found',
 			github: 'https://github.com/stacy89/Lost_and_Found',
 			image: 'public/Projects/Lost&Found.png',
 			description: "A social network app that allows people to travel and visit restaurants and bars like a local in a new city, instead of just being a tourist. This app was built after my own struggle to find real, authentic experiences while traveling.",
 			tech: "Tech: Sinatra, JavaScript,  Google Maps API"
 		},
 		{
+			name: 'Authentic Realities',
 			live: 'https://proj-authentic-realities.herokuapp.com/index.html',
 			github: 'https://github.com/stacy89/Authentic-Realities',
 			image: 'public/Projects/AuthenticRealities.png',
