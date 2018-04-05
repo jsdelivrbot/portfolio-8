9c5526cb4d1bb242d751437093aaf6c088e288d2
Stacy Areas <stacyareas@gmail.com>
2018-04-04 23:57:21 -0700

    test

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -37,21 +37,6 @@ angular.module('portfolio.projects', ['ngRoute'])
 			tech: "Tech:  JavaScript, DialogFlow, Firebase"
 		},
 		{
-			name: 'Lost and Found',
-			github: 'https://github.com/stacy89/Lost_and_Found',
-			image: 'public/Projects/Lost&Found.png',
-			description: "A social network app that allows people to travel and visit restaurants and bars like a local in a new city, instead of just being a tourist. This app was built after my own struggle to find real, authentic experiences while traveling.",
-			tech: "Tech: Sinatra, JavaScript,  Google Maps API"
-		},
-		{
-			name: 'Authentic Realities',
-			live: 'https://proj-authentic-realities.herokuapp.com/index.html',
-			github: 'https://github.com/stacy89/Authentic-Realities',
-			image: 'public/Projects/AuthenticRealities.png',
-			description: "Data visualization using VR to express data/statistics of inequalities in salary based on personal identity. Created the main visual environment and helped with coding the triggered events on collision. This experience is offered to help users feels an emotional connection to the lived experience of socioeconomic status.",
-			tech: "Tech: Unity, C#, MonoDevelop, Heroku, SteamVR, Oculus"
-		},
-		{
 			name: 'Student MGMT',
 			live: 'https://teacherstudentmanagement.herokuapp.com/',
 			github: 'https://github.com/stacy89/Student-Management-Tool',
@@ -59,5 +44,20 @@ angular.module('portfolio.projects', ['ngRoute'])
 			description: "An administrative system to store student data and generate student groups based on criteria like GPA, gender, and attendance. Formulated the students routes and used CSS to design the pages. This tool is useful for teachers that have a hard time forming groups of their students and to keep track of what student belongs to what teacher.   (Login: teacher@teacher.com, Password: teacher)",
 			tech: "Tech: Ruby on Rails, Ruby, PostgreSQL, Heroku, Twilio API"
 		}
+		// {
+		// 	name: 'Authentic Realities',
+		// 	live: 'https://proj-authentic-realities.herokuapp.com/index.html',
+		// 	github: 'https://github.com/stacy89/Authentic-Realities',
+		// 	image: 'public/Projects/AuthenticRealities.png',
+		// 	description: "Data visualization using VR to express data/statistics of inequalities in salary based on personal identity. Created the main visual environment and helped with coding the triggered events on collision. This experience is offered to help users feels an emotional connection to the lived experience of socioeconomic status.",
+		// 	tech: "Tech: Unity, C#, MonoDevelop, Heroku, SteamVR, Oculus"
+		// },
+		// {
+		// 	name: 'Lost and Found',
+		// 	github: 'https://github.com/stacy89/Lost_and_Found',
+		// 	image: 'public/Projects/Lost&Found.png',
+		// 	description: "A social network app that allows people to travel and visit restaurants and bars like a local in a new city, instead of just being a tourist. This app was built after my own struggle to find real, authentic experiences while traveling.",
+		// 	tech: "Tech: Sinatra, JavaScript,  Google Maps API"
+		// }
 	];
 }]);
